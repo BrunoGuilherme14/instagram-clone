a009953abea1c9972178bb30d932714886dda344
Bruno Guilherme Ferreira da Silva <bgfs@intranet.pitang.com>
2019-09-12 12:39:18 -0300

    Animação - Utilziando Keyframses

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-acesso',
@@ -18,15 +18,24 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
       transition('void => criado', animate('0.5s 0.5s ease-in-out'))
     ]),
     trigger('painel', [
-      state('void', style({
-        transform: 'translateX(50px)',
-        opacity: 0
-      })),
       state('criado', style({
         transform: 'translateX(0)',
         opacity: 1
       })),
-      transition('void => criado', animate('0.5s 0.5s ease-in-out'))
+      transition('void => criado', [
+        style({transform: 'translateX(50px)', opacity: 0 }),
+        animate('1.5s 0s ease-in-out', keyframes([
+          style({ offset:0.15, opacity: 1, transform: 'translateX(0)'  }),
+          style({ offset:0.85, opacity: 1, transform: 'translateX(0)'  }),
+          style({ offset:0.86, opacity: 1, transform: 'translateY(-5px)' }),
+          style({ offset:0.90, opacity: 1, transform: 'translateY(5px)' }),
+          style({ offset:0.92, opacity: 1, transform: 'translateY(-5px)' }),
+          style({ offset:0.94, opacity: 1, transform: 'translateY(5px)' }),
+          style({ offset:0.96, opacity: 1, transform: 'translateY(-5px)' }),
+          style({ offset:0.98, opacity: 1, transform: 'translateY(5px)' }),
+          style({ offset:1, opacity: 1, transform: 'translateY(0)' })
+        ]))
+      ])
     ])
   ]
 })
