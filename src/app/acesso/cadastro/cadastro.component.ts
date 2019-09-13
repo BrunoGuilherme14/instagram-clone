a662f1bfb4d84c115dca47b52f00306fdfdc855e
Bruno Guilherme Ferreira da Silva <bgfs@intranet.pitang.com>
2019-09-13 12:03:46 -0300

    Forms - Validação de formulário e função submit

--- src/app/acesso/cadastro/cadastro.component.ts
+++ src/app/acesso/cadastro/cadastro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Cadastro } from './cadastro.model';
 
 @Component({
   selector: 'app-cadastro',
@@ -9,10 +10,10 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class CadastroComponent implements OnInit {
   @Output('exibir') exibir: EventEmitter<string> = new EventEmitter<string>();
   public formCadastro: FormGroup = new FormGroup({
-    'email': new FormControl(null, [Validators.required, Validators.minLength(10)]),
-    'nome': new FormControl(null, [Validators.required, Validators.minLength(5)]),
+    'email': new FormControl(null, [Validators.required, Validators.email]),
+    'nome': new FormControl(null, [Validators.required, Validators.minLength(5), Validators.pattern("[a-zA-Z ]*")]),
     'usuario': new FormControl(null, [Validators.required]),
-    'senha': new FormControl(null, [Validators.required]),
+    'senha': new FormControl(null, [Validators.required, Validators.minLength(6)]),
   });
   constructor() { }
 
@@ -22,6 +23,10 @@ export class CadastroComponent implements OnInit {
     this.exibir.emit('login')
   }
   public cadastrar(): void {
-    console.log('form cadastro')
+    this.formCadastro.markAllAsTouched()
+    if(this.formCadastro.valid) {
+      const formValues: Cadastro = new Cadastro(this.formCadastro.value.email, this.formCadastro.value.nome, this.formCadastro.value.usuario, this.formCadastro.value.senha)
+      console.log(formValues)
+    }
   }
 }
---
+++ src/app/acesso/cadastro/cadastro.model.ts
@@ -0,0 +1,8 @@
+export class Cadastro {
+    constructor(
+        public email:string,
+        public nome:string,
+        public usuario: string,
+        public senha: string
+    ){}
+}
