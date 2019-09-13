c7c5685b1f0d757ce365ad13b578289086d8d690
Bruno Guilherme Ferreira da Silva <bgfs@intranet.pitang.com>
2019-09-13 18:02:14 -0300

    Rotas - Configurando rotas da aplicação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PublicacoesComponent } from './publicacoes/publicacoes.component';
+import { AcessoComponent } from './acesso/acesso.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AcessoComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'publicacoes', component: PublicacoesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
