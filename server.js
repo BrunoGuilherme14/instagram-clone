f75358bca4a1c9e3dc7e408ef4d0d220226c28f4
Bruno Guilherme Ferreira da Silva <bgfs@intranet.pitang.com>
2019-09-25 13:46:37 -0300

    Preparando ambiente para heroku

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const nomeApp = process.env.npm_package_name;
+const app = express();
+ 
+app.use(express.static('./dist/instagram-clone'));
+ 
+app.get('/*', (req, res) => {
+    res.sendFile(path.join('./dist/instagram-clone/index.html'));
+});
+ 
+app.listen(process.env.PORT || 8080);
