f1aa383f0732b4802af40de0c5f22d43d7ff4d75
Bruno Guilherme Ferreira da Silva <bgfs@intranet.pitang.com>
2019-09-27 09:57:44 -0300

    Alterando server.js para heroku

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const nomeApp = process.env.npm_package_name;
 const app = express();
  
-app.use(express.static('./dist/instagram-clone'));
+app.use(express.static('./dist/instagram-clone/'));
  
 app.get('/*', (req, res) => {
-    res.status(200).sendFile(path.join('./dist/instagram-clone/index.html'));
+    res.status(200).sendFile(path.join(__dirname ,'./dist/instagram-clone/index.html'));
 });
  
 app.listen(process.env.PORT || 8080);
